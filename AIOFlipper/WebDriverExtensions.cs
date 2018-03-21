68f98ccd22b47810510b09b80d1438ddb028bea2
Sacha <sacha.vandenwyngaert@student.ap.be>
2018-03-21 23:12:27 +0100

    Comment out window pushback

--- AIOFlipper/WebDriverExtensions.cs
+++ AIOFlipper/WebDriverExtensions.cs
@@ -144,27 +144,27 @@ namespace AIOFlipper
         {
             IWebDriver driver = targetLocator.Window(windowName);
 
-            try
-            {
-                foreach (Process process in SetWindowPosition.GetPrimaryProcesses("chrome"))
-                {
-                    if (process.Id != 1816)
-                    {
-                        SetWindowPosition.ForceWindowToStayOnBottom(process);
-                        Console.WriteLine("Successfully pushed back a chrome window with PID: " + process.Id);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Left chrome window with PID " + process.Id + " untouched.");
-                    }
-                }
+            //try
+            //{
+            //    foreach (Process process in SetWindowPosition.GetPrimaryProcesses("chrome"))
+            //    {
+            //        if (process.Id != 1816)
+            //        {
+            //            SetWindowPosition.ForceWindowToStayOnBottom(process);
+            //            Console.WriteLine("Successfully pushed back a chrome window with PID: " + process.Id);
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Left chrome window with PID " + process.Id + " untouched.");
+            //        }
+            //    }
 
-                Console.WriteLine("Successfully pushed back the chrome windows.");
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Failed to push back the chrome windows.");
-            }
+            //    Console.WriteLine("Successfully pushed back the chrome windows.");
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("Failed to push back the chrome windows.");
+            //}
 
             return driver;
         }
